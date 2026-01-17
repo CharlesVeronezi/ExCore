09182cf55488816e3e2d83ee2708a1c5835223ce
CharlesVeronezi <charlesmayer002@gmail.com>
2026-01-16 21:00:52 -0300

    Adicionado migrations etapa 1

---
+++ src/Backend/SagiCore.Infrastructure/Migrations/DatabaseVersion.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SagiCore.Infrastructure.Migrations
+{
+    public abstract class DatabaseVersion
+    {
+        public const int TABLE_USER = 1;
+    }
+}
---
+++ src/Backend/SagiCore.Infrastructure/Migrations/Versions/Version0000001.cs
@@ -0,0 +1,18 @@
+﻿using FluentMigrator;
+
+namespace SagiCore.Infrastructure.Migrations.Versions
+{
+    [Migration(DatabaseVersion.TABLE_USER, "Criação das tabelas de produto, pedido de venda e itens do pedido de venda")]
+    internal class Version0000001 : Migration
+    {
+        public override void Down()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Up()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
